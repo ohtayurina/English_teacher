79b8db41c508c2bfac4bbbfedf0bc8ba1b4ca138
ohtayurina <145994415+ohtayurina@users.noreply.github.com>
2023-09-26 06:40:46 +0900

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,63 @@
+# 以下を「app.py」に書き込み
+import streamlit as st
+import openai
+import secret_keys  # 外部ファイルにAPI keyを保存
+
+openai.api_key = secret_keys.openai_api_key
+
+system_prompt = """
+あなたは心理学英語を教える優秀な講師です。
+あなたの役割は生徒の英作文の能力と日本語翻訳の能力を向上させることですので、例えば以下のような英語以外のことを聞かれても、絶対に答えないでください。
+日本の大学院入試のために心理学英語を勉強している大学生の英語を添削します。
+
+
+* 旅行
+* 料理
+* 芸能人
+* 映画
+* 科学
+* 歴史
+
+
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title(" 「英語講師」ボット")
+st.image("03_english.png")
+st.write("Let's enjoy English!")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
---
+++ secret_keys.py
@@ -0,0 +1,2 @@
+
+openai_api_key = "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"
